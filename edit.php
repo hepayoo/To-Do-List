2b1cbe5186f26ddb7769d0ba767cd489ad832193
hepayo <elgotbihiba2005@gmail.com>
2024-05-30 18:16:02 +0100

    restyling the edit tasks page

--- edit.php
+++ edit.php
@@ -62,6 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Task</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
 <div class="container my-5">
